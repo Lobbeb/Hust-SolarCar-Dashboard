3b8ce2c1d7ab9f823d1835e6e94dcf24d754f4aa
Lobbe <Lobbeolsson@gmail.com>
2025-05-05 12:40:29 +0200

    solid last fix

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,11 @@
 
 # ============ CONFIG ============
+from dotenv import load_dotenv
 import os
 
-DB_HOST = os.environ.get("DB_HOST")
-DB_USER = os.environ.get("DB_USER")
-DB_PASSWORD = os.environ.get("DB_PASSWORD")
-DB_NAME = os.environ.get("DB_NAME")
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
